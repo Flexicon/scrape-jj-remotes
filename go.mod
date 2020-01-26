7b2ba44a8edcf6eb30a32a48dd8e99d1f105e85e
Michal Repec <mikerepec@gmail.com>
2020-01-26 23:04:28 +0100

    Implement external api caching

--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/go-redis/redis/v7 v7.0.0-beta.5 // indirect
 	github.com/labstack/echo/v4 v4.1.13
 	github.com/labstack/gommon v0.3.0
+	github.com/patrickmn/go-cache v2.1.0+incompatible
 )
--- pkg/fetch/fetch.go
+++ pkg/fetch/fetch.go
@@ -3,28 +3,35 @@ package fetch
 import (
 	"encoding/json"
 	"github.com/flexicon/scrape-jj-remotes/pkg/model"
+	"github.com/patrickmn/go-cache"
 	"io/ioutil"
 	"net/http"
 	"time"
 )
 
-const apiUrl = "https://justjoin.it/api/offers"
+const (
+	apiUrl         = "https://justjoin.it/api/offers"
+	apiResponseKey = "JJ_API_RESPONSE"
+)
 
 type JobFetchingService struct {
 	httpClient *http.Client
+	cache      *cache.Cache
 }
 
 func NewJobFetchingService() *JobFetchingService {
-	httpClient := &http.Client{
-		Timeout: time.Second * 5,
-	}
-
 	return &JobFetchingService{
-		httpClient: httpClient,
+		httpClient: &http.Client{Timeout: time.Second * 5},
+		cache:      cache.New(5*time.Minute, 10*time.Minute),
 	}
 }
 
 func (s *JobFetchingService) Fetch() ([]model.Job, error) {
+	cachedResponse, found := s.cache.Get(apiResponseKey)
+	if found {
+		return s.interpretResponse(cachedResponse.([]byte))
+	}
+
 	res, err := s.httpClient.Get(apiUrl)
 	if err != nil {
 		return nil, err
@@ -35,19 +42,25 @@ func (s *JobFetchingService) Fetch() ([]model.Job, error) {
 		return nil, err
 	}
 
+	return s.interpretResponse(buf)
+}
+
+func (s *JobFetchingService) interpretResponse(response []byte) ([]model.Job, error) {
 	var jobs []model.Job
-	err = json.Unmarshal(buf, &jobs)
+	err := json.Unmarshal(response, &jobs)
 	if err != nil {
 		return nil, err
 	}
 
-	return onlyRemoteJobs(jobs), nil
+	s.cache.Set(apiResponseKey, response, cache.DefaultExpiration)
+
+	return onlyRemoteNonB2BJobs(jobs), nil
 }
 
-func onlyRemoteJobs(jobs []model.Job) []model.Job {
+func onlyRemoteNonB2BJobs(jobs []model.Job) []model.Job {
 	var i int
 	for _, job := range jobs {
-		if job.Remote {
+		if job.Remote && job.EmploymentType != "b2b" {
 			jobs[i] = job
 			i++
 		}
